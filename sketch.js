e9c9b95f002fd6d48fa0d8c8e239df97481bf8db
realshafer <realshafer@gmail.com>
2024-04-20 13:26:58 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,83 +1,153 @@
-/* For mobile phones - accesses accelerometer and gyroscope.
-Make sure you turn on orientation lock on your iPhone or Android device. */
+let theta1=0
+let theta2=0
+let theta3=0
 
-let alpha = 0, beta = 0 , gamma = 0; // gyroscope variables
-let x = 0.0, y = 0.0, z = 0.0 ; // accelerometer variables
-let bunnyImage;
-let xPosition = 0;
-let yPosition = 0;
+let dthetaXdt=0;
+let dthetaYdt=0;
+let dthetaZdt=0;
+
+let dtheta1dt=0;
+let dtheta2dt=0;
+let dtheta3dt=0;
+
+let thetaX=0;
+let thetaY=0;
+let thetaZ=0;
+
+let det=false
+
+let t=0,dt=1;
+
+let bob = 0, sarah = 0 , jeff = 0; // gyroscope variables
+
+function cb(event){
+  dthetaXdt=event.rotationRate.alpha*PI/180;
+  dthetaYdt=event.rotationRate.beta*PI/180;
+  dthetaZdt=event.rotationRate.gamma*PI/180;
+  
+  let ct=millis()/1000;
+  dt=ct-t;
+  t=ct;
+
+  det=true;
+}
+
+function onClick(){
+  if (typeof DeviceMotionEvent.requestPermission === 'function') {
+    DeviceMotionEvent.requestPermission()
+      .then(permissionState => {
+        if (permissionState === 'granted') {
+          window.addEventListener('devicemotion', cb);
+        }
+      })
+      .catch(console.error);
+  } else {
+    window.addEventListener('devicemotion', cb);
+    // handle regular non iOS 13+ devices
+  }
+}
+
 
 
 
 function setup() {
-
-  createCanvas(windowWidth, windowHeight);
-
-  bunnyImage = loadImage("assets/bunny.jpg");
-  imageMode(CENTER);
-  rectMode(CENTER);
-
+  createCanvas(400, 400);
 }
 
-function draw() {
-
-  background('#c6f5ff'); // light blue
-
-  // the map command !!!!
-  // takes your variable and maps it from range 1 to range 2
-  // map(yourVar, range1_x, range1_y, range2_x, range2_y) ;
-  xPosition = map(gamma, -60, 60, 0, width);
-  yPosition = map(beta, -30, 30, 0, height);
-
-  push(); // before you use translate, rotate, or scale commands, push and then pop after
-
-  translate(xPosition, yPosition); // move everything over by x, y
-
-  rotate(radians(alpha)); // rotate the bunny depending on the alpha intake
-
-  image(bunnyImage, 0, 0, 500, 500);
-  // rect(0, 0, 100, 100) ;
-  pop();
-
-
+function draw(){
+  dthetaXdt = map(bob, -60, 60, 0, width);
+  dthetaYdt = map(sarah, -30, 30, 0, height);
+  dthetaZdt = map(jeff, -30, 30, 0, height);
+  
   // Text commands that display debugging data
   textAlign(LEFT);
   textSize(20);
-  fill('black');
+  fill('rgb(255,255,255)');
   text("orientation data:", 25, 25);
   textSize(15);
-  text("alpha: " + alpha, 25, 50);
-  text("beta: " + beta, 25, 70);
-  text("gamma: " + gamma, 25, 90);
-  textSize(20);
-  text("acceleration data:", 25, 125);
-  textSize(15);
-  text("x = " + x.toFixed(2), 25, 150); // .toFixed means just show (x) decimal places
-  text("y = " + y.toFixed(2), 25, 170);
-  text("z = " + z.toFixed(4), 25, 190);
-
-  // Text that makes CTK type in the background
-  fill('white');
-  noStroke();
-  textSize(300);
-  textAlign(CENTER);
-  text("ctk", width / 2, height / 2);
-
+  text("x: " + bob, 25, 50);
+  text("y: " + sarah, 25, 70);
+  text("z: " + jeff, 25, 90);
 }
 
 
 
-// Read in gyroscope data
-window.addEventListener('deviceorientation', function(e) {
-  alpha = e.alpha;
-  beta = e.beta;
-  gamma = e.gamma;
-});
+// function draw(){
+ 
+//   background(0);
+  
+//   directionalLight(255,255,255,-1,-1,-1);
+//   ambientLight(80);
 
-// Read in accelerometer data
-window.addEventListener('devicemotion', function(e) {
-  // get accelerometer values
-  x = e.acceleration.x;
-  y = e.acceleration.y;
-  z = e.acceleration.z;
-});
+//   dtheta1dt=dthetaXdt;
+//   dtheta2dt=dthetaYdt*sin(thetaX)+dthetaZdt*cos(thetaX);
+//   dtheta3dt=-dthetaYdt*cos(thetaX)*cos(theta2)
+//     +dthetaZdt*sin(thetaX)*cos(theta2)
+  
+
+//   scale(1.5)
+
+  
+//   stroke(0)
+
+
+//   let r1=100,r2=80,r3=60;
+
+//   noStroke()
+//   let detailX=48*2;
+//   let detailY=32;
+
+//   //outer ring
+//   torus(r1,5,detailX,detailY)
+
+//   push()
+//   translate((r1+r2)/2,0,0)
+//   rotateZ(PI/2)
+//   cylinder(5,20)
+//   pop()
+//   push()
+//   translate(-(r1+r2)/2,0,0)
+//   rotateZ(PI/2)
+//   cylinder(5,20)
+//   pop()
+
+//   //middle ring
+//   rotateX(theta1)
+//   rotateX(PI/2)
+//   torus(80,5,detailX,detailY)
+
+//   push()
+//   translate(0,(r2+r3)/2,0)
+//   rotateY(PI/2)
+//   cylinder(5,20)
+//   pop()
+
+//   push()
+//   translate(0,-(r2+r3)/2,0)
+//   rotateY(PI/2)
+//   cylinder(5,20)
+//   pop()
+
+
+//   //inner ring
+//   rotateY(theta2);
+//   rotateY(PI/2)
+//   torus(r3,5,detailX,detailY)
+
+//   //box
+//   rotateX(theta3);
+//   box(50)
+//   rotateZ(PI/2)
+//   cylinder(5,2*r3)
+
+//   if(det && dtheta1dt==dtheta1dt){
+//     theta1+=dtheta1dt*dt;
+//     theta2+=dtheta2dt*dt;
+//     theta3+=dtheta3dt*dt;
+    
+//     thetaX+=dthetaXdt*dt;
+//     thetaY+=dthetaYdt*dt;
+//     thetaZ+=dthetaZdt*dt;
+//   }
+
+// }
